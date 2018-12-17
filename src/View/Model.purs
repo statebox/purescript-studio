6e3de08f30805c468b8e437fb1a5734c9ab11ca1
Erik Post <erik@shinsetsu.nl>
2018-12-17 14:12:18 +0100

    Typedefs editor initial work. #64 (#75)

--- src/View/Model.purs
+++ src/View/Model.purs
@@ -2,14 +2,17 @@ module View.Model where
 
 import Prelude
 import Data.Auth (RoleInfo)
+import Data.Tuple.Nested (type (/\), (/\))
 import View.Diagram.Model (DiagramInfo)
 import View.Petrinet.Model (NetInfo)
+import View.Typedefs.TypedefsEditor (Typedef2(..))
 
 type Project =
   { name         :: String
   , nets         :: Array NetInfo
   , diagrams     :: Array DiagramInfo
   , allRoleInfos :: Array RoleInfo
+  , types        :: Array (String /\ Typedef2)
   }
 
 type Diagram = Unit
