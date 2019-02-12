6e4c8cfca092a2ac0d160e88d6f5e20425a58f6a
André Videla <andre.videla@gmail.com>
2019-02-12 14:31:49 +0100

    Decode NLL to BrickDiagram and DiagramInfo  (#118, PR #120)
    * Add transformation from NLL to Directed graph
    
    * Add basic test
    
    * delete old file
    
    * Rempalce forall and module path
    
    * Fix fromNLL
    
    * add cyclic check
    
    * implement naive transformation to DiagramInfo
    
    * Replace enumerate by mapWithIndex
    
    * Correctly adjust the width for consecutive elements
    
    * update docs and api
    
    * Update tests for diagrams encoding
    
    * update docs and formatting
    
    * Move FromNLL file to Data/Diagram
    
    * A few minor formatting/comment edits. #118

--- test/Main.purs
+++ test/Main.purs
@@ -3,17 +3,17 @@ module Test.Main where
 import Prelude
 
 import Data.Either (Either(..))
+import Data.Petrinet.Representation.NLL (ErrNetEncoding(..))
+import Data.Petrinet.Representation.NLL as Net
 import Data.Tuple (Tuple(..))
+import Data.Vec2D (vec3)
 import Effect (Effect)
 import Effect.Class.Console (log)
-
 import Test.Spec (pending, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (run)
-
-import Data.Petrinet.Representation.NLL as Net
-import Data.Petrinet.Representation.NLL (ErrNetEncoding(..))
+import View.Diagram.FromNLL as Diagram
 
 main :: Effect Unit
 main = run [consoleReporter] do
@@ -23,3 +23,19 @@ main = run [consoleReporter] do
     it "reject odd length encodings" do
       Net.fromNLL 0 [1,2,0,3,0,3,0] `shouldEqual` Left ErrOddLength
     pending "infer a single trailing zero?"
+  describe "Brick diagram encoding" do
+    it "decodes a simple graph" do
+       Diagram.fromNLL [2, 2, 1, 8, 8, 8, 3] "test" `shouldEqual`
+           Right { name: "test"
+                 , ops: [ { identifier: "0:0", pos: vec3 0 0 1, label: "2" }
+                        , { identifier: "0:1", pos: vec3 0 1 1, label: "1" }
+                        , { identifier: "1:0", pos: vec3 1 0 2, label: "8" }
+                        , { identifier: "2:0", pos: vec3 2 0 1, label: "8" }
+                        , { identifier: "2:1", pos: vec3 2 1 1, label: "3" }
+                        ]
+                 }
+
+    it "should fail on cyclic graphs" do
+       Diagram.fromNLL [1, 1,2,3,1] "test" `shouldEqual` Left Diagram.ErrGraphIsCyclic
+--    it "should fail on multiple graphs with one cyclic" do
+--       Diag.fromNLL [2,2,5,3,5,4,6,0,5] "test" `shouldEqual` Left Diag.ErrGraphIsCyclic
