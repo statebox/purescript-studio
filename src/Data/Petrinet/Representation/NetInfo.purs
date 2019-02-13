97ad586e0fc0c2f709a93b276ca88b60cb9e76a3
André Videla <andre.videla@gmail.com>
2019-02-13 14:33:37 +0000

    add builder functions for NetRep

--- src/Data/Petrinet/Representation/NetInfo.purs
+++ src/Data/Petrinet/Representation/NetInfo.purs
@@ -1,10 +1,14 @@
 module Data.Petrinet.Representation.NetInfo where
 
+import Data.Monoid
 import Data.Petrinet.Representation.Dict
 import Data.Petrinet.Representation.NLL
-import Data.Map (Map)
-import Data.Monoid
 import View.Petrinet.Model
+
+import Data.Map (Map)
+import Data.Auth (Role, Roles, RoleInfo)
+import Data.Tuple.Nested (type (/\), (/\))
+import Data.Vec3 (Vec2, Vec2D)
 import Prelude ((<<<), identity)
 
 mkNetInfo :: String -> NetObj -> NetApi -> Array TextBox -> NetInfo
@@ -21,7 +25,31 @@ parseNetObj = mkNetObjF <<< getNetRep
     parseMarking :: NetF a -> Marking
     parseMarking = mkMarkingF <<< markingMap
     getNetRep :: NetF a -> NetRep
-    getNetRep netF = mkNetRep [] [] (parseMarking netF) [] [] [] [] [] []
+    getNetRep netF = mkNetRep (mkArrayPID netF) 
+                              (mkArrayTrans netF) 
+                              (parseMarking netF) 
+                              (mkArrayPIDString netF)
+                              (mkArrayPIDVec netF) 
+                              (mkArrayString netF) 
+                              (mkArrayTypedef netF) 
+                              (mkArrayVec2D netF)
+                              (mkArrayRoles netF)
+    mkArrayPID :: NetF a -> Array PID
+    mkArrayPID netF = []
+    mkArrayTrans :: NetF a -> Array Transition
+    mkArrayTrans netF = []
+    mkArrayPIDString :: NetF a -> Array (PID /\ String)
+    mkArrayPIDString netF = []
+    mkArrayPIDVec :: NetF a -> Array (PID /\ Vec2D)
+    mkArrayPIDVec net = []
+    mkArrayString :: NetF a -> Array String
+    mkArrayString net = []
+    mkArrayTypedef :: NetF a -> Array Typedef
+    mkArrayTypedef net = []
+    mkArrayVec2D :: NetF a -> Array Vec2D
+    mkArrayVec2D net = []
+    mkArrayRoles :: NetF a -> Array Roles
+    mkArrayRoles net = []
 
 
 parseNetApi :: ∀ a. NetF a -> NetApi
