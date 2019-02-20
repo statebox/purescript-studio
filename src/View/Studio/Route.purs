5123e0f33aa69030d3473b3bbe442114430c7d04
Erik Post <erik@shinsetsu.nl>
2019-02-20 15:49:18 +0000

    Merge initial Statebox API support #86
    * Initial stbx-js API support. #86
    
    * Move transaction decoding into Statebox.API. #86
    
    * Introduce initial transition/namespace. #86
    
    * Mark menu entry types with a letter for now. #86
    
    * Render diagrams fetched from API. #86 #118
    
    * ObjectTree component: automatically assign tree node id's. #86

--- src/View/Studio/Route.purs
+++ src/View/Studio/Route.purs
@@ -3,15 +3,25 @@ module View.Studio.Route where
 import Prelude
 import Data.Maybe (Maybe)
 import View.Model (ProjectName)
+import Statebox.API.Types (HashStr, PathElem, URL)
 
 type Route = RouteF ProjectName DiagramName NetName
 
 data RouteF p d n
   = Home
-  | Net     p n
-  | Diagram p d (Maybe (NodeIdent d n)) -- ^ A diagram with maybe one of its 'child' nodes.
-  | Types   p
-  | Auths   p
+  | Types      p
+  | Auths      p
+
+  -- Project-related constructors
+  | Net        p n
+  | Diagram    p d (Maybe (NodeIdent d n)) -- ^ A diagram with maybe one of its 'child' nodes.
+
+  -- Statebox API-related constructors
+  | NamespaceR HashStr
+  | WiringR    WiringFiringInfo
+  | FiringR    WiringFiringInfo
+  | DiagramR   HashStr PathElem String
+  | NetR       HashStr PathElem String
 
 derive instance eqRouteF :: (Eq p, Eq d, Eq n) => Eq (RouteF p d n)
 derive instance ordRouteF :: (Ord p, Ord d, Ord n) => Ord (RouteF p d n)
@@ -20,9 +30,34 @@ type DiagramName = String
 
 type NetName = String
 
+data ResolvedRouteF p d n
+  = ResolvedHome
+  | ResolvedNet       n
+  | ResolvedDiagram   d (Maybe (NodeIdent d n))
+  | ResolvedTypes     p
+  | ResolvedAuths     p
+
+  -- Statebox API-specific constructors
+  | ResolvedNamespace HashStr
+  | ResolvedWiring    WiringFiringInfo
+  | ResolvedFiring    WiringFiringInfo
+
 --------------------------------------------------------------------------------
 
 data NodeIdent d n = DiagramNode d | NetNode n
 
 derive instance eqNodeIdent :: (Eq d, Eq n) => Eq (NodeIdent d n)
 derive instance ordNodeIdent :: (Ord d, Ord n) => Ord (NodeIdent d n)
+
+--------------------------------------------------------------------------------
+
+type WiringFiringInfo =
+  { name        :: String
+  , endpointUrl :: URL
+  , hash        :: HashStr
+  }
+
+type NamespaceInfo =
+  { name :: String
+  , hash :: HashStr
+  }
