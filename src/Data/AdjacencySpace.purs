76aa41cba89d802b65a58e99ae53d9cb0d4d9992
Erik Post <erik@shinsetsu.nl>
2019-03-06 17:23:42 +0100

    Clean up transaction tree code. #143

--- src/Data/AdjacencySpace.purs
+++ src/Data/AdjacencySpace.purs
@@ -11,7 +11,8 @@ import Data.Maybe (Maybe(..), maybe, fromMaybe, isJust)
 import Data.Set as Set
 import Data.Set (Set)
 
--- | Adjacency data structure. Records ids, the values corresponding to those ids, and the links between ids and parent ids.
+-- | Doubly linked adjacency data structure. Records ids, the (optional) values corresponding to those ids,
+-- | and the links between keys and their parent and child keys.
 type AdjacencySpace k v =
   { kids      :: Map k (Set k)
   , parents   :: Map k k
@@ -30,18 +31,19 @@ lookup k t = Map.lookup k t.values
 -- TODO optimisation: unify parents and values? maintaining two indices that are the same seems expensive.
 update :: ∀ a. (a -> Key) -> AdjacencySpace Key a -> Key -> a -> AdjacencySpace Key a
 update getParent t curKey val =
-  t { kids    = Map.alter          (Just <<< Set.insert curKey <<< fromMaybe mempty) parent t.kids
-    , parents = Map.insert curKey parent                                                  t.parents
-    , values  = Map.insert curKey val                                                     t.values
+  t { kids    = Map.alter  updateKids parent t.kids
+    , parents = Map.insert curKey     parent t.parents
+    , values  = Map.insert curKey     val    t.values
     }
   where
     parent = getParent val
+    updateKids = Just <<< Set.insert curKey <<< fromMaybe mempty
 
 kids :: ∀ f v. Monoid (Set Key) => AdjacencySpace Key v -> Key -> Set Key
-kids t hash = fromMaybe mempty (Map.lookup hash t.kids)
+kids t k = fromMaybe mempty (Map.lookup k t.kids)
 
--- | The tree roots of the forest of known hashes. The values corresponding to these
--- | hashes may be either loaded or unloaded.
+-- | The tree roots of the forest of known keys. The values corresponding to these
+-- | keys may be either loaded or unloaded.
 rootKeys :: ∀ v. AdjacencySpace Key v -> Set Key
 rootKeys t = Set.filter (not isLoaded) parentKeys
   where
@@ -61,15 +63,15 @@ isLoadedIn t h = h `member` t.values
 
 -- | - TODO This does a recursive call which will at some point blow the stack; look at buildCofree.
 -- | - TODO This function assumes the given `AdjacencySpace` encodes a tree.
--- |        Computation will diverge in the presence of cycles among the hash codes and their parents.
+-- |        Computation will diverge in the presence of cycles among the keys and their parents/children.
 unsafeToTree
   :: forall v b
    . (AdjacencySpace Key v -> Key -> Maybe v -> Array (Cofree Array b) -> Cofree Array b)
   -> AdjacencySpace Key v
   -> Key
   -> Cofree Array b
-unsafeToTree mkNode t hash =
-  mkNode t hash (Map.lookup hash t.values) nodeKids
+unsafeToTree mkNode t k =
+  mkNode t k (Map.lookup k t.values) nodeKids
   where
     nodeKids :: Array (Cofree Array b)
-    nodeKids = unsafeToTree mkNode t <$> Set.toUnfoldable (kids t hash)
+    nodeKids = unsafeToTree mkNode t <$> Set.toUnfoldable (kids t k)
