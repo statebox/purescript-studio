6ffe9680cd36a35d88bf74e6deb159bc537b1f8a
Erik Post <erik@shinsetsu.nl>
2019-05-14 12:53:21 +0200

    Remove hack to load stbx.js via a require in FFI code. #159

--- src/Statebox/API/Execution.js
+++ src/Statebox/API/Execution.js
@@ -2,13 +2,6 @@
 
 // TODO assume stbx.js loaded globally; warn if absent??
 
-// TODO remove this; hacked in to make tests work on node.js
-// TODO remove this; hacked in to make tests work on node.js
-// TODO remove this; hacked in to make tests work on node.js
-// var Stbx = require('/Users/erik/dev/statebox/stbx-core-js/src/index.js')
-var Stbx = require('../../node_modules/@statebox/stbx-js/dist/stbx.min.js')
-// console.log('Stbx =', Stbx)
-
 function fromWiring (wiring) {
   return new Stbx(wiring)
 }
