89c771d7b99feabc8c6d9c1240e5488ce2605d96
Marco Perone <pasafama@gmail.com>
2019-06-12 18:39:17 +0200

    stbx-example-data: use correct type for transaction hash #206

--- stbx-example-data/src/Transactions.purs
+++ stbx-example-data/src/Transactions.purs
@@ -1,4 +1,6 @@
 module Transactions where
 
-transactionByJelle20190528 :: String
+import Statebox.Core.Transaction (TxId)
+
+transactionByJelle20190528 :: TxId
 transactionByJelle20190528 = "zFsGM27HNS66qmGp1Y1STK48FUA1F12VHLRB51RGWNYWV"
