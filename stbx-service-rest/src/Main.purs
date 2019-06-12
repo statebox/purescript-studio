31d12103fc3876bb00bbfa8fbdf840eb66a2634b
Marco Perone <pasafama@gmail.com>
2019-06-12 19:02:37 +0200

    stbx-service-rest: add /tx/<hash> endpoint #204

--- stbx-service-rest/src/Main.purs
+++ stbx-service-rest/src/Main.purs
@@ -8,8 +8,19 @@ import Node.Express.App (App, listenHttp, get)
 import Node.Express.Response (send)
 import Node.HTTP (Server)
 
+healthcheck :: App
+healthcheck = get "/healthcheck" $ send ""
+
+-- | Transaction endpoint
+-- | responds to /tx/<hash>
+-- | returns a json-encoded Stbx.Core.Transaction.Tx
+transaction :: App
+transaction = get "/tx/:hash" $ send "tx"
+
 app :: App
-app = get "/" $ send "Hello, Marco!"
+app = do
+  healthcheck
+  transaction
 
 main :: Effect Server
 main = do
