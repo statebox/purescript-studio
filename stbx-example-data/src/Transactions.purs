289d7518a28d24f9b938b92cd6130684dc076191
Marco Perone <pasafama@gmail.com>
2019-06-12 23:59:02 +0200

    Create stbx-example-data Spago module. #206
    * setup npm and spago for example-spago-module #206
    
    * add output to gitignore for stbx-example-data
    
    * add transaction hash example
    
    * stbx-example-data: add .spago to gitignore #206
    
    * stbx-example-app: add stbx-core dependency #206
    
    * stbx-example-data: use correct type for transaction hash #206
    
    * add white lines at the end of files

---
+++ stbx-example-data/src/Transactions.purs
@@ -0,0 +1,6 @@
+module Transactions where
+
+import Statebox.Core.Transaction (TxId)
+
+transactionByJelle20190528 :: TxId
+transactionByJelle20190528 = "zFsGM27HNS66qmGp1Y1STK48FUA1F12VHLRB51RGWNYWV"
