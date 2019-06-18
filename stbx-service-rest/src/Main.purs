803366de589a8e9a7a433b33172a8b22e9914a28
Marco Perone <pasafama@gmail.com>
2019-06-18 10:16:49 +0200

    stbx-service-rest: add logger and error handler #204

--- stbx-service-rest/src/Main.purs
+++ stbx-service-rest/src/Main.purs
@@ -4,16 +4,32 @@ import Prelude
 
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Effect.Exception (error)
-import Node.Express.App (App, listenHttp, get)
-import Node.Express.Handler (Handler, nextThrow)
-import Node.Express.Request (getRouteParam)
-import Node.Express.Response (send)
+import Effect.Exception (Error, error, message)
+import Node.Express.App (App, listenHttp, get, use, useOnError)
+import Node.Express.Handler (Handler, next, nextThrow)
+import Node.Express.Request (getRouteParam, getOriginalUrl)
+import Node.Express.Response (sendJson, setStatus)
 import Node.HTTP (Server)
 
+-- middleware
+
+logger :: Handler
+logger = do
+  url <- getOriginalUrl
+  liftEffect $ log (">>>" <> url)
+  next
+
+errorHandler :: Error -> Handler
+errorHandler err = do
+  setStatus 400
+  sendJson {error: message err}
+
+-- handlers
+
 healthcheck :: Handler
-healthcheck = send ""
+healthcheck = sendJson {health: "I'm fine"}
 
 -- | Transaction endpoint
 -- | responds to /tx/<hash>
@@ -23,12 +39,16 @@ transaction =  do
   maybeHash <- getRouteParam "hash"
   case maybeHash of
     Nothing -> nextThrow $ error "Hash is required"
-    Just hash -> send hash
+    Just hash -> sendJson {hash: hash}
+
+-- application definition with routing
 
 app :: App
 app = do
+  use                logger
   get "/healthcheck" healthcheck
   get "/tx/:hash"    transaction
+  useOnError         errorHandler
 
 main :: Effect Server
 main = do
