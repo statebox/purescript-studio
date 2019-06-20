15f957e983b19392cb91b36e8354b36a90daf3df
Marco Perone <pasafama@gmail.com>
2019-06-20 09:43:56 +0200

    stabx-service-rest: integrate in-memory transactions handling #204

--- stbx-service-rest/src/Main.purs
+++ stbx-service-rest/src/Main.purs
@@ -2,11 +2,14 @@ module Main where
 
 import Prelude
 
+import Control.Monad.State.Trans (runStateT)
+import Data.Map (empty)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Aff.Class (liftAff)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
+import Effect.Ref (Ref, new, read)
 import Effect.Exception (Error, error, message)
 import Node.Express.App (App, listenHttp, get, use, useOnError)
 import Node.Express.Handler (Handler, next, nextThrow)
@@ -14,13 +17,18 @@ import Node.Express.Request (getRouteParam, getOriginalUrl)
 import Node.Express.Response (sendJson, setStatus)
 import Node.HTTP (Server)
 
-import Model (getTransaction, loggingActions)
-
--- data
+import Model (TransactionDictionary, getTransaction, inMemoryActions)
 
 -- TODO: move this elsewhere
 endpointUrl :: String
 endpointUrl = "https://testapi.statebox.io"
+
+-- application state
+
+type AppState = Ref TransactionDictionary
+
+initState :: Effect AppState
+initState = new empty
 
 -- middleware
 
@@ -53,28 +61,32 @@ healthcheck = sendJson {health: "I'm fine"}
 -- | getTransaction endpoint
 -- | responds to /tx/<hash>
 -- | returns a json-encoded Stbx.Core.Transaction.Tx
-getTransactionHandler :: Handler
-getTransactionHandler =  do
+getTransactionHandler :: AppState -> Handler
+getTransactionHandler appState = do
   maybeHash <- getRouteParam "hash"
   case maybeHash of
     Nothing   -> nextThrow $ error "Hash is required"
     Just hash -> do
-      maybeTransaction <- liftAff $ loggingActions $ getTransaction hash
+      transactionDictionary <- liftEffect $ read appState
+      maybeTransaction <- liftAff $ runStateT (inMemoryActions $ getTransaction hash) transactionDictionary
       sendJson { hash: hash
                , transaction : maybeTransaction
                }
 
 -- application definition with routing
 
-app :: App
-app = do
+app :: AppState -> App
+app state = do
   use                logger
   get "/"            index
   get "/healthcheck" healthcheck
-  get "/tx/:hash"    getTransactionHandler
+  get "/tx/:hash"    (getTransactionHandler state)
   useOnError         errorHandler
+
+-- run application
 
 main :: Effect Server
 main = do
-  listenHttp app 8080 \_ ->
+  state <- initState
+  listenHttp (app state) 8080 \_ ->
     log $ "Listening on " <> show 8080
