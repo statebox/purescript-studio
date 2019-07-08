2ba11fc0e38a1e83e8424e0cc7c43cbf526cd7b1
Erik Post <erik@shinsetsu.nl>
2019-07-08 21:43:19 +0200

    halogen-petrinet-editor-example: Show multiple nets. #222

--- halogen-petrinet-editor-example/src/Main.purs
+++ halogen-petrinet-editor-example/src/Main.purs
@@ -1,16 +1,25 @@
 module Main where
 
 import Prelude
+import Data.Traversable (traverse)
 import Effect (Effect)
-import Halogen.Aff (awaitBody, runHalogenAff)
+import Halogen.Aff (awaitBody, awaitLoad, runHalogenAff)
+import Halogen.Aff.Util (selectElement)
 import Halogen.VDom.Driver (runUI)
+import Record (union)
+import Web.DOM.ParentNode (QuerySelector(..))
 
 import ExampleData as Ex
-import Record (union)
-
 import View.Petrinet.PetrinetEditor as PetrinetEditor
 
 main :: Effect Unit
 main = runHalogenAff do
-  body <- awaitBody
-  runUI PetrinetEditor.ui (Ex.netInfo1 `union` { roleInfos: [], types: [] }) body
+  awaitLoad
+
+  let net1 = Ex.netInfo1 `union` { roleInfos: [], types: [] }
+  net1ElemMaybe <- selectElement (QuerySelector "#net1")
+  _ <- runUI PetrinetEditor.ui net1 `traverse` net1ElemMaybe
+
+  let net2 = Ex.netInfo2 `union` { roleInfos: [], types: [] }
+  net2ElemMaybe <- selectElement (QuerySelector "#net2")
+  runUI PetrinetEditor.ui net2 `traverse` net2ElemMaybe
