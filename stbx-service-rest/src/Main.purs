9450cc3c5806ac0f6646fd652ed952140c15f5cf
Marco Perone <pasafama@gmail.com>
2019-07-11 13:59:09 +0200

    stbx-service-rest: Create  API transactions endpoint '/tx' (WIP) (#213)
    * add development commands for stbx-service-rest
    
    * add express npm dependency to stbx-service-rest #204
    
    * first version of a working server for stbx-service-rest #204
    
    * stbx-service-rest: add /tx/<hash> endpoint #204
    
    * stbx-service-rest: use url parameter #204
    
    * stbx-service-rest: separate route definition from handler #204
    
    * stbx-service-rest: add logger and error handler #204
    
    * stbx-service-rest: add index endpoint #204
    
    * stbx-client-rest: add xhr2 as an nm dependency #204
    
    * stbx-service-rest: add xhr2 as a npm dependency #204
    
    * stbx-service-rest: add stbx-client-rest as a dependency #204
    
    * stbx-service-rest: implement getTransaction as a proxy #204
    
    * stbx-service-rest: create mock version of domain #204
    
    * stbx-service-rest: integrate new model in get transaction handler #204
    
    * stbx-service-rest: use type classes intead of using directly Aff #204
    
    * stbx-service-rest: create in-memory version for model #204
    
    * stbx-service-rest: --watch option on spago run does not work well #204
    
    * stabx-service-rest: integrate in-memory transactions handling #204
    
    * stbx-service-rest: rename PutHashTransaction to AddTransactionData #204
    
    * stbx-service-rest: setup POST /tx endpoint #204
    
    * stbx-service-rest: upgrade to spago 0.8.5 #204
    
    * stbx-client-rest: upgrade to spago 0.8.5 #204

--- stbx-service-rest/src/Main.purs
+++ stbx-service-rest/src/Main.purs
@@ -2,9 +2,112 @@ module Main where
 
 import Prelude
 
+import Control.Monad.Except (runExcept)
+import Control.Monad.State.Trans (runStateT)
+import Data.Either (Either(..))
+import Data.Function.Uncurried (Fn3)
+import Data.Map (empty)
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Aff.Class (liftAff)
+import Effect.Class (liftEffect)
 import Effect.Console (log)
+import Effect.Ref (Ref, new, read)
+import Effect.Exception (Error, error, message)
+import Foreign (F)
+import Node.Express.App (App, listenHttp, get, post, use, useExternal, useOnError)
+import Node.Express.Handler (Handler, next, nextThrow)
+import Node.Express.Request (getBody, getRouteParam, getOriginalUrl)
+import Node.Express.Response (sendJson, setStatus)
+import Node.Express.Types (Request, Response)
+import Node.HTTP (Server)
 
-main :: Effect Unit
+import Model (TransactionDictionary, getTransaction, inMemoryActions)
+
+-- import body parser
+foreign import jsonBodyParser :: Fn3 Request Response (Effect Unit) (Effect Unit)
+
+-- TODO: move this elsewhere
+endpointUrl :: String
+endpointUrl = "https://testapi.statebox.io"
+
+-- application state
+
+type AppState = Ref TransactionDictionary
+
+initState :: Effect AppState
+initState = new empty
+
+-- middleware
+
+logger :: Handler
+logger = do
+  url <- getOriginalUrl
+  liftEffect $ log (">>>" <> url)
+  next
+
+errorHandler :: Error -> Handler
+errorHandler err = do
+  setStatus 400
+  sendJson {error: message err}
+
+-- handlers
+
+index :: Handler
+index = sendJson
+  { name        : "Statebox could API"
+  , description : "collection of endpoints to interact with the Statebox protocol"
+  , endpoints   :
+    { healthcheck    : "/healthcheck"
+    , getTransaction : "/tx/:hash"
+    }
+  }
+
+healthcheck :: Handler
+healthcheck = sendJson {health: "I'm fine"}
+
+-- | getTransaction endpoint
+-- | responds to GET /tx/<hash>
+-- | returns a json-encoded Stbx.Core.Transaction.Tx
+getTransactionHandler :: AppState -> Handler
+getTransactionHandler appState = do
+  maybeHash <- getRouteParam "hash"
+  case maybeHash of
+    Nothing   -> nextThrow $ error "Hash is required"
+    Just hash -> do
+      transactionDictionary <- liftEffect $ read appState
+      maybeTransaction <- liftAff $ runStateT (inMemoryActions $ getTransaction hash) transactionDictionary
+      sendJson { hash: hash
+               , transaction : maybeTransaction
+               }
+
+-- | postTransaction endpoint
+-- | responds to POST /tx
+postTransactionHandler :: AppState -> Handler
+postTransactionHandler appState = do
+  fBody :: F (Array String) <- getBody
+  case runExcept fBody of
+    Left errors -> sendJson { status: "ko"
+                            , errors : show errors
+                            }
+    Right hash  -> sendJson { status: "ok" }
+
+-- application definition with routing
+
+app :: AppState -> App
+app state = do
+  useExternal         jsonBodyParser
+  use                 logger
+  get  "/"            index
+  get  "/healthcheck" healthcheck
+  get  "/tx/:hash"    (getTransactionHandler state)
+  post "/tx"          (postTransactionHandler state)
+  useOnError          errorHandler
+
+-- run application
+
+main :: Effect Server
 main = do
-  log "🍝"
+  state <- initState
+  listenHttp (app state) 8080 \_ ->
+    log $ "Listening on " <> show 8080
---
+++ stbx-service-rest/src/Model.purs
@@ -0,0 +1,60 @@
+module Model where
+
+import Prelude
+
+import Control.Monad.Free (Free, liftF, runFreeM)
+import Control.Monad.Rec.Class (class MonadRec)
+import Control.Monad.State.Class (class MonadState, get, modify_)
+import Data.Map (Map, insert, lookup)
+import Data.Maybe (Maybe(..))
+import Effect.Class (class MonadEffect)
+import Effect.Class.Console (log)
+
+import Statebox.Core.Transaction (Tx, TxSum)
+
+-- define possible actions
+
+type AddTransactionData =
+  { hash        :: String
+  , transaction :: Tx TxSum
+  }
+
+data ActionsF a
+  = GetTransaction String (Maybe (Tx TxSum) -> a)
+  | AddTransaction AddTransactionData a
+
+derive instance functorActions :: Functor (ActionsF)
+
+type Actions = Free ActionsF
+
+-- define basic interactions
+
+getTransaction :: String -> Actions (Maybe (Tx TxSum))
+getTransaction txHash = liftF $ GetTransaction txHash identity
+
+putTransaction :: String -> Tx TxSum -> Actions Unit
+putTransaction hash transaction = liftF $ AddTransaction {hash: hash, transaction: transaction} unit
+
+-- logging instance
+
+loggingActions :: forall a m. MonadEffect m => MonadRec m => Actions a -> m a
+loggingActions = runFreeM $ \action -> case action of
+  GetTransaction txHash next      -> do
+    log $ "get transaction " <> txHash
+    pure $ next Nothing
+  AddTransaction transaction next -> do
+    log $ "put transaction" <> show transaction
+    pure next
+
+-- in-memory instance
+
+type TransactionDictionary = Map String (Tx TxSum)
+
+inMemoryActions :: forall a m. MonadRec m => MonadState TransactionDictionary m => Actions a -> m a
+inMemoryActions = runFreeM $ \action -> case action of
+  GetTransaction txHash next -> do
+    transactionsMap <- get
+    pure $ next $ lookup txHash transactionsMap
+  AddTransaction {hash: hash, transaction: transaction} next -> do
+    modify_ $ insert hash transaction
+    pure next
