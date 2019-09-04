7c29868ccb13fc9fee7dfabf42acfa627ea951a6
Erik Post <erik@shinsetsu.nl>
2019-09-04 19:57:15 +0200

    Remove a comment to trigger Travis build.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,3 @@
-# todo npm install in een andere reeks doen?
-
 cd studio                          && npm install && npm run build
 cd ..
 cd stbx-service-rest               && npm install && npm run build
