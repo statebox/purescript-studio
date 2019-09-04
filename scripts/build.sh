bc6e6b25c9f41a0e93b4b3336d95832955bf1fe0
Erik Post <erik@shinsetsu.nl>
2019-09-04 19:44:56 +0200

    Add basic Travis build. #246

---
+++ scripts/build.sh
@@ -0,0 +1,8 @@
+# todo npm install in een andere reeks doen?
+
+cd studio                          && npm install && npm run build
+cd ..
+cd stbx-service-rest               && npm install && npm run build
+cd ..
+cd halogen-petrinet-editor-example && npm install && npm run build
+cd ..
