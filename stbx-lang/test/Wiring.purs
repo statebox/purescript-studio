060c1861b670aa1f211d11e2cdf38a4875990314
Sjoerd Visscher <sjoerd@q42.nl>
2019-11-13 14:58:16 +0100

    Wiring parser

--- stbx-lang/test/Wiring.purs
+++ stbx-lang/test/Wiring.purs
@@ -1,6 +1,7 @@
 module Test.Language.Statebox.Wiring where
 
 import Prelude
+import Data.Bifunctor (lmap)
 import Data.List as List
 import Data.List (List(..))
 import Data.String (trim)
@@ -8,17 +9,22 @@ import Effect (Effect)
 import Effect.Class.Console (log)
 import Language.Statebox as Statebox
 import Language.Statebox.Hypergraph (NodeF(..), HyperEdgeF(..), GElemF(..))
-import Language.Statebox.Wiring.AST (Node(..), GElem(..), HyperEdge(..), Label)
+import Language.Statebox.Wiring.AST (Node(..), GElem(..), HyperEdge(..), Label, stripSpan)
 import Test.Spec                  (Spec, describe, pending, it)
 import Test.Spec.Console          (write)
 import Test.Spec.Runner           (runSpec)
 import Test.Spec.Assertions       (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 
+import Debug.Trace (spy)
+
 spec :: Spec _
 spec = do
   describe "Statebox wiring compiler" do
-    pending "Implement wiring tests."
+    it "should parse wirings correctly" do
+      let ast = Statebox.parseWiring wiring1src
+      let astDump = spy "ast" $ show $ ast
+      (ast # map (map (lmap stripSpan))) `shouldEqual` pure wiring1expected
 
 wiring1src :: String
 wiring1src = trim """
