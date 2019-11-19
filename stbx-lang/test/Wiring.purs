bb4cade069822b3eba9323c9d30012977540dd1a
Sjoerd Visscher <sjoerd@q42.nl>
2019-11-19 11:15:16 +0100

    Add wiring -> diagram test
    Fixes #270

--- stbx-lang/test/Wiring.purs
+++ stbx-lang/test/Wiring.purs
@@ -9,19 +9,18 @@ import Language.Statebox as Statebox
 import Language.Statebox.Hypergraph (HyperEdgeF(..), GElemF(..))
 import Language.Statebox.Wiring.AST (Label, stripSpan)
 import Language.Statebox.Wiring.Generator.Diagram (toDiagramWithName)
+import Statebox.Core.Types (Diagram)
 import Test.Spec                  (Spec, describe, it)
 import Test.Spec.Assertions       (shouldEqual)
-
-import Debug.Trace (spy)
 
 spec :: Spec Unit
 spec = do
   describe "Statebox wiring compiler" do
     it "should parse wirings correctly" do
       let ast = Statebox.parseWiring wiring1src
-      let astDump = spy "ast" $ show $ ast
-      let diagram1 = spy "diagram1" $ toDiagramWithName "dummy" <$> ast
+      let diagram1 = toDiagramWithName "dummy" <$> ast
       (ast # map (map (lmap stripSpan))) `shouldEqual` pure wiring1expected
+      diagram1 `shouldEqual` pure diagram1expected
 
 wiring1src :: String
 wiring1src = trim """
@@ -39,6 +38,13 @@ wiring1expected = mkAst
   , mkEdge ["c1"] ["d1"]
   ]
 
+diagram1expected :: Diagram
+diagram1expected =
+  { name: "dummy"
+  , width: 5
+  , pixels: [ 1, 1, 2, 3, 5, 2, 3, 4, 5, 4 ]
+  , names: [ "a1", "b1", "b2", "d1", "c1" ]
+  }
 
 --------------------------------------------------------------------------------
 -- graph DSL
