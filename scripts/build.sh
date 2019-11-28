6309702bef167562e85bfda97c610d476b0199c3
Erik Post <erik@shinsetsu.nl>
2019-11-28 15:31:25 +0100

    stbx-service-rest: Create and enable integration tests #271 (#277)
    * stbx-service-rest: Create and enable client-server integration tests. #271
    
    * stbx-service-rest: Fix permissions on CI test suite. #271
    
    * stbx-service-rest: Create PureScript test suite for tx API POSTs and GETs. #271
    
    * Chain tests with && so one failure will fail the whole suite. #271
    
    * Make sure projects in test suite are built before they are tested. #271
    
    * stbx-service-rest: Add index-test.js test runner. #271

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,10 @@
-cd studio                          && npm install && npm run build
-cd ..
-cd stbx-service-rest               && npm install && npm run build
-cd ..
-cd halogen-petrinet-editor-example && npm install && npm run build
+cd studio                          && npm install && npm run build && \
+cd .. && \
+cd stbx-core                       && npm install && npm run build && \
+cd .. && \
+cd stbx-service-rest               && npm install && npm run build && \
+cd .. && \
+cd stbx-lang                       && npm install && npm run build && \
+cd .. && \
+cd halogen-petrinet-editor-example && npm install && npm run build && \
 cd ..
