ccecf30f4cad52994687a35ad95e75fe6c6831c3
Erik Post <erik@shinsetsu.nl>
2019-11-28 14:40:57 +0100

    Make sure projects in test suite are built before they are tested. #271

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 cd studio                          && npm install && npm run build && \
 cd .. && \
+cd stbx-core                       && npm install && npm run build && \
+cd .. && \
 cd stbx-service-rest               && npm install && npm run build && \
 cd .. && \
 cd stbx-lang                       && npm install && npm run build && \
