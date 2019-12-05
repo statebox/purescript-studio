bb9e0ac6d9c9906e792db0e15c921fc00c9783db
Sjoerd Visscher <sjoerd@w3future.com>
2019-12-05 12:56:10 +0100

    Use names in terms view

--- src/View/Term.purs
+++ src/View/Term.purs
@@ -49,7 +49,7 @@ render :: ∀ m. Input -> H.ComponentHTML Action () m
 render { term, selection: { path, count } } = div [ classes [ ClassName "term" ] ] (rec (Just path) term) where
   rec :: Maybe Path -> TTerm -> Array (H.ComponentHTML Action () m)
   rec p (Fix (Ann _ TUnit)) = [ div [ clsSel p "tunit" ] [ i_ [ text "I" ] ] ]
-  rec p (Fix (Ann ty (TBox { bid, decl }))) = [ div [ clsSel p "tbox" ] [ format ty decl bid ] ]
+  rec p (Fix (Ann ty (TBox { name, decl }))) = [ div [ clsSel p "tbox" ] [ format ty decl name ] ]
   rec p (Fix (Ann _ (TC terms))) = [ div [ clsSel p "tc" ] $ intercalate [ div_ [ text "⊙" ] ] $ withPath p terms ]
   rec p (Fix (Ann _ (TT terms))) = [ div [ clsSel p "tt" ] $ intercalate [ div_ [ text "⊗" ] ] $ withPath p terms ]
   clsSel (Just Nil) n = classes [ ClassName n, ClassName "selected" ]
@@ -62,9 +62,9 @@ render { term, selection: { path, count } } = div [ classes [ ClassName "term" ]
 format :: ∀ m. Ty String -> TypeDecl String -> String -> H.ComponentHTML Action () m
 format _ (Perm []) _ = i_ [ text "I" ]
 format (Ty vs _) (Perm p) _ | take (length p) [1,2,3,4,5,6,7,8,9] == p = i_ [ text "id", formatTy vs "⊗" ]
-format (Ty vs _) (Perm _) bid = i_ [ text bid, formatTy vs "," ]
-format (Ty l r) (Spider _ _ _) bid = i_ [ text bid, formatTy (l <> r # head # fromFoldable) "" ]
-format _ _ bid = span_ [ text bid ]
+format (Ty vs _) (Perm _) name = i_ [ text name, formatTy vs "," ]
+format (Ty l r) (Spider _ _ _) name = i_ [ text name, formatTy (l <> r # head # fromFoldable) "" ]
+format _ _ name = span_ [ text name ]
 
 formatTy :: ∀ m. Array String -> String -> H.ComponentHTML Action () m
 formatTy vs sep = sub_ [ text (intercalate sep vs) ]
