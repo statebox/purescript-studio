ad3697addb27bd70d9d73640ba706a4fecc8f790
Sjoerd Visscher <sjoerd@w3future.com>
2019-12-16 21:12:55 +0100

    Add grid spacing slider

--- halogen-graphical/html/index.js
+++ halogen-graphical/html/index.js
@@ -1,12 +1,13 @@
 var Main = require("../output/Main")
 var runHalogenAff = require("../output/Halogen.Aff.Util").runHalogenAff
 function f(form) {
+  var spacing = Math.pow(10, form.spacing.value)
   var s = Math.pow(10, form.scale.value);
   var x = form.x.value;
   var y = form.y.value;
   document.getElementById("canvas").innerHTML = "";
   runHalogenAff(Main.run({
-    gridSpacing: 20,
+    gridSpacing: spacing,
     range: {
       topLeft: { x: (-0.5 - x)*s, y: (-0.5 - y)*s },
       bottomRight: { x: (0.5 - x)*s, y: (0.5 - y)*s }
