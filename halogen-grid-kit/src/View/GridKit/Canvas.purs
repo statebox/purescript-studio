5d88f4524242f5b401ab7dd6b4a15f0f6c019fad
Sjoerd Visscher <sjoerd@w3future.com>
2019-12-31 13:57:18 +0100

    Use more affine transforms in Canvas

--- halogen-grid-kit/src/View/GridKit/Canvas.purs
+++ halogen-grid-kit/src/View/GridKit/Canvas.purs
@@ -3,21 +3,15 @@ module View.GridKit.Canvas where
 import Prelude
 
 import Data.Array ((..), filter)
-import Data.Bifunctor (bimap)
-import Data.Maybe
 import Data.Int (floor, ceil, toNumber)
-import Data.Vec3 (Vec2, _x, _y, Box(..), origin2)
+import Data.Vec3 (Vec2, _x, _y, origin2, point2, Box, boxSize, boxCenter)
 import Data.Vec3.AffineTransform
-import Effect (Effect)
-import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Class (class MonadEffect)
 import Halogen as H
 import Halogen.HTML hiding (code, head, prop, map, div)
-import Halogen.HTML.Properties (ref)
-import Halogen.Query.Input (RefLabel(..))
 import Math (log, pow, ln10, round, sqrt)
 import Svg.Elements as S
 import Svg.Attributes hiding (path) as S
-import Web.DOM.Element (Element, setAttribute)
 
 import View.ReactiveInput as RI
 
@@ -41,38 +35,35 @@ ui =
     }
 
 render :: ∀ m. Input -> {} -> H.ComponentHTML Void () m
-render { gridSpacing, range: range@(Box { topLeft, bottomRight }), size } _ =
-  S.svg [ S.width (_x size), S.height (_y size), S.viewBox x1 y1 (x2 - x1) (y2 - y1) ]
+render { gridSpacing, range, size } _ =
+  S.svg [ S.width (_x size), S.height (_y size) ]
   [ S.g [ S.attr (AttrName "class") "grid grid-v" ] $
-      gridLines gridSpacing pixelSize x1 x2
-        # map \{ width, pos } -> S.line [ S.strokeWidth width, S.x1 pos, S.y1 y1, S.x2 pos, S.y2 y2 ]
+      gridLines spacing (_x topLeft) (_x bottomRight)
+        # map \{ width, pos } -> let x = m2c_x pos in S.line [ S.strokeWidth width, S.x1 x, S.y1 0.0, S.x2 x, S.y2 (_y size) ]
   , S.g [ S.attr (AttrName "class") "grid grid-h" ] $
-      gridLines gridSpacing pixelSize y1 y2
-        # map \{ width, pos } -> S.line [ S.strokeWidth width, S.x1 x1, S.y1 pos, S.x2 x2, S.y2 pos ]
+      gridLines spacing (_y topLeft) (_y bottomRight)
+        # map \{ width, pos } -> let y = m2c_y pos in S.line [ S.strokeWidth width, S.x1 0.0, S.y1 y, S.x2 (_x size), S.y2 y ]
   ]
   where
-    scaleXY = size / (bottomRight - topLeft)
+    scaleXY = size / boxSize range
     scaleMin = min (_x scaleXY) (_y scaleXY)
-    pixelSize = 1.0 / scaleMin
-    rangeCenter = (topLeft + bottomRight) / pure 2.0
+    spacing = gridSpacing / scaleMin
+    rangeCenter = boxCenter range
     canvasCenter = size / pure 2.0
+    canvas2model = translate rangeCenter * scale (1.0 / scaleMin) * translate (-canvasCenter)
     model2canvas = translate canvasCenter * scale scaleMin * translate (-rangeCenter)
-    canvas2model = translate rangeCenter * scale pixelSize * translate (-canvasCenter)
-    topLeft' = canvas2model `transform` origin2
-    bottomRight' = canvas2model `transform` (origin2 + size)
-    x1 = _x topLeft'
-    y1 = _y topLeft'
-    x2 = _x bottomRight'
-    y2 = _y bottomRight'
+    m2c_x x = _x (model2canvas `transform` point2 x 0.0)
+    m2c_y y = _y (model2canvas `transform` point2 0.0 y)
+    topLeft = canvas2model `transform` origin2
+    bottomRight = canvas2model `transform` (origin2 + size)
 
 type GridLine = { pos :: Number, width :: Number }
-gridLines :: Number -> Number -> Number -> Number -> Array GridLine
-gridLines pixelSpacing pixelSize start end
+gridLines :: Number -> Number -> Number -> Array GridLine
+gridLines spacing start end
   = (ceil (start / stepSize) .. floor (end / stepSize))
-  # map (\n -> { pos: toNumber n * stepSize, width: pixelSize * (w n - thresholdWidth) })
+  # map (\n -> { pos: toNumber n * stepSize, width: w n - thresholdWidth })
   # filter (\{ width } -> width > 0.0)
   where
-    spacing = pixelSpacing * pixelSize
     stepSize = pow 10.0 (round (log (spacing / stepMultiplyer) / ln10))
     zoom = stepSize / spacing
     w 0 = maxLineWidth
--- vec/src/Data/Vec3/Box.purs
+++ vec/src/Data/Vec3/Box.purs
@@ -1,12 +1,15 @@
 module Data.Vec3.Box
   ( Box(..)
   , BoxRecF
+  , boxSize
+  , boxCenter
   , mapVec3s
   , toArray
   ) where
 
 import Prelude
 import Data.Newtype (class Newtype, over)
+import Data.Ring (class Ring)
 
 import Data.Vec3.Vec3
 
@@ -21,6 +24,12 @@ type BoxRecF a =
   { topLeft     :: a
   , bottomRight :: a
   }
+
+boxSize :: ∀ a. Ring a => Box a -> Vec3 a
+boxSize (Box { topLeft, bottomRight }) = bottomRight - topLeft
+
+boxCenter :: Box Number -> Vec3 Number
+boxCenter (Box { topLeft, bottomRight }) = (topLeft + bottomRight) / pure 2.0
 
 map :: ∀ a b. (a -> b) -> BoxRecF a -> BoxRecF b
 map f {topLeft, bottomRight} = {topLeft: f topLeft, bottomRight: f bottomRight}
