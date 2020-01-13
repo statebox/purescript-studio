99e2260997ce4addabebd04f3d42d98f5fbc9e9f
Marco Perone <pasafama@gmail.com>
2020-01-13 15:25:52 +0100

    [#273] [stbx-protocol] handle uber root and initial transaction

--- stbx-protocol/src/Statebox/Protocol.purs
+++ stbx-protocol/src/Statebox/Protocol.purs
@@ -1,13 +1,24 @@
 module Statebox.Protocol where
 
-import Data.Unit (Unit)
+import Prelude
+import Data.Either (Either(..))
 
-import Statebox.Core.Transaction (HashStr, TxSum(..))
-import Statebox.TransactionStore.Types (Actions)
+import Statebox.Core.Transaction (HashStr, InitialTx, TxId, TxSum(..), isUberRootHash)
+import Statebox.TransactionStore.Types (Actions, put)
 
-processTxSum :: HashStr -> TxSum -> Actions Unit
+data ProcessError
+  = NoUberRoot
+  | InitialPreviousNoUberRoot TxId
+
+processTxSum :: HashStr -> TxSum -> Either ProcessError (Actions Unit)
 processTxSum hash = case _ of
-  UberRootTxInj           -> ?u
-  InitialTxInj  initialTx -> ?i
+  UberRootTxInj           -> Left NoUberRoot
+  InitialTxInj  initialTx -> processInitialTx hash initialTx
   WiringTxInj   wiringTx  -> ?w
   FiringTxInj   firingTx  -> ?f
+
+processInitialTx :: HashStr -> InitialTx -> Either ProcessError (Actions Unit)
+processInitialTx hash initialTx =
+  if (isUberRootHash initialTx.previous)
+  then Right $ put hash $ InitialTxInj initialTx
+  else Left $ InitialPreviousNoUberRoot initialTx.previous
