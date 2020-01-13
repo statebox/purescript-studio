e5ed460e8bd9cf9e6d707f5808774826d05ed25e
Marco Perone <pasafama@gmail.com>
2020-01-13 17:28:47 +0100

    [#273] [stbx-protocol] process wiring transaction

--- stbx-protocol/src/Statebox/Protocol.purs
+++ stbx-protocol/src/Statebox/Protocol.purs
@@ -2,23 +2,42 @@ module Statebox.Protocol where
 
 import Prelude
 import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
 
-import Statebox.Core.Transaction (HashStr, InitialTx, TxId, TxSum(..), isUberRootHash)
-import Statebox.TransactionStore.Types (Actions, put)
+import Statebox.Core.Transaction (HashStr, InitialTx, TxId, TxSum(..), WiringTx, evalTxSum, isUberRootHash)
+import Statebox.TransactionStore.Types (Actions, get, put)
 
 data ProcessError
   = NoUberRoot
   | InitialPreviousNoUberRoot TxId
+  | WiringNotPreviousInitial  TxId
 
-processTxSum :: HashStr -> TxSum -> Either ProcessError (Actions Unit)
+processTxSum :: HashStr -> TxSum -> Actions (Either ProcessError Unit)
 processTxSum hash = case _ of
-  UberRootTxInj           -> Left NoUberRoot
+  UberRootTxInj           -> pure $ Left NoUberRoot
   InitialTxInj  initialTx -> processInitialTx hash initialTx
-  WiringTxInj   wiringTx  -> ?w
+  WiringTxInj   wiringTx  -> processWiringTx  hash wiringTx
   FiringTxInj   firingTx  -> ?f
 
-processInitialTx :: HashStr -> InitialTx -> Either ProcessError (Actions Unit)
+processInitialTx :: HashStr -> InitialTx -> Actions (Either ProcessError Unit)
 processInitialTx hash initialTx =
   if (isUberRootHash initialTx.previous)
-  then Right $ put hash $ InitialTxInj initialTx
-  else Left $ InitialPreviousNoUberRoot initialTx.previous
+  then map Right $ put hash $ InitialTxInj initialTx
+  else pure $ Left $ InitialPreviousNoUberRoot initialTx.previous
+
+isInitialTx :: TxId -> Actions Boolean
+isInitialTx hash = do
+  maybeTxSum <- get hash
+  case maybeTxSum of
+    Nothing -> pure false
+    Just tx -> pure $ evalTxSum (const false) (const true) (const false) (const false) tx
+
+processWiringTx :: HashStr -> WiringTx -> Actions (Either ProcessError Unit)
+processWiringTx hash wiringTx =
+  let
+    previousHash = wiringTx.previous
+  in do
+    isPreviousInitial <- isInitialTx previousHash
+    if isPreviousInitial
+    then map Right $ put hash $ WiringTxInj wiringTx
+    else pure $ Left $ WiringNotPreviousInitial hash
