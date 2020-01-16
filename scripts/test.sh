580a2f8595881b92a6ff9506ff7699192bb02e31
Erik Post <erik@shinsetsu.nl>
2020-01-16 18:29:01 +0100

    [#294] [stbx-rest-integration] fix scripts/test.sh.

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,5 +4,5 @@ set -e
 cd vec                   && npm test && cd ..
 cd stbx-core             && npm test && cd ..
 cd stbx-service-rest     && npm test && cd ..
-cd stbx-rest-integration && npm run test
+cd stbx-rest-integration && npm test && cd ..
 cd stbx-lang             && npm test && cd ..
