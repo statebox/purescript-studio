e31adbeca0f12fc4ee54c5f4b16380c1bf2c02e4
Marco Perone <pasafama@gmail.com>
2020-01-22 11:33:04 +0100

    [#273] [stbx-core] add _firingExecution traversal

--- stbx-core/src/Statebox/Core/Lenses.purs
+++ stbx-core/src/Statebox/Core/Lenses.purs
@@ -1,10 +1,10 @@
 module Statebox.Core.Lenses where
 
 import Prelude
-import Data.Lens (Prism', prism', Lens', lens)
+import Data.Lens (Lens', Prism', Traversal', lens, prism', _Just)
 import Data.Maybe (Maybe(..))
 import Data.NonEmpty (singleton, head)
-import Statebox.Core.Transaction (TxSum(..), WiringTx, FiringTx, isExecutionTx)
+import Statebox.Core.Transaction (TxId, TxSum(..), WiringTx, FiringTx, isExecutionTx)
 import Statebox.Core.Types (Firing, GluedTransitionIdRaw)
 
 --------------------------------------------------------------------------------
@@ -25,6 +25,12 @@ _executionTx = prism' FiringTxInj $ case _ of FiringTxInj x | isExecutionTx x ->
 
 _firing :: Lens' FiringTx Firing
 _firing = lens (_.firing) (_ { firing = _ })
+
+_execution :: Lens' Firing (Maybe TxId)
+_execution = lens (_.execution) (_ {  execution = _ })
+
+_firingExecution :: Traversal' FiringTx TxId
+_firingExecution = _firing <<< _execution <<< _Just
 
 _firingPath :: Lens' Firing GluedTransitionIdRaw
 _firingPath = lens (_.path >>> head) (\r x -> r { path = singleton x })
