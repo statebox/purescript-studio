577bf0c565be5976c3d393486150a0a7f7d8de05
Marco Perone <pasafama@gmail.com>
2020-02-04 12:46:51 +0100

    [#328] [stbx-protocol] implement transition firing

--- stbx-protocol/src/Statebox/Protocol/ExecutionState.purs
+++ stbx-protocol/src/Statebox/Protocol/ExecutionState.purs
@@ -1,8 +1,10 @@
 module Statebox.Protocol.ExecutionState where
 
 import Statebox.Core.Transaction (TxId)
+import View.Petrinet.Model (Marking)
 
 type ExecutionState =
   { lastFiring :: TxId
   , wiring     :: TxId
+  , marking    :: Marking
   }
--- stbx-protocol/src/Statebox/Protocol/Fire.purs
+++ stbx-protocol/src/Statebox/Protocol/Fire.purs
@@ -1,6 +1,44 @@
 module Statebox.Protocol.Fire where
 
-import Statebox.Core.Transaction (FiringTx)
+import Prelude
+import Data.Array (index)
+import Data.Either (Either(..), either)
+import Data.Either.Nested (type (\/))
+import Data.Maybe (maybe)
+import Data.NonEmpty (head)
 
--- fire :: ∀ a . FiringTx -> a
--- fire = ?fire
+import Data.Petrinet.Representation.Dict (fireAtMarking)
+import Statebox.Core.Transition (gluedTokens)
+import Statebox.Core.Types (Firing, Wiring)
+import Statebox.Core.WiringTree (LinearizationError, fromWiring, linearize)
+import View.Petrinet.Model (Marking)
+
+data FiringError
+
+  -- | The wiring does not describe a valid wiring tree.
+  = FireInvalidWiringTree
+
+  -- | The linerization of the wiring tree failed with a LinerizationError
+  | FireLinerizationError LinearizationError
+
+  -- | The path of the firing describing
+  | FireTransitionIndexOutOfBounds
+
+  -- | The selected transition is not enabled
+  | FireTransitionNotEnabled
+
+fire :: Wiring -> Firing -> Marking -> FiringError \/ Marking
+fire wiring firing marking = maybe
+  (Left FireInvalidWiringTree)
+  (\wiringTree ->
+    either
+      (Left <<< FireLinerizationError)
+      (\gluedTransitions ->
+        let transitionIndex = head firing.path
+        in maybe
+          (Left FireTransitionIndexOutOfBounds)
+          (\gluedTransition ->
+            Right $ fireAtMarking marking $ gluedTokens gluedTransition)
+          (index gluedTransitions transitionIndex))
+      (linearize wiringTree))
+  (fromWiring wiring)
