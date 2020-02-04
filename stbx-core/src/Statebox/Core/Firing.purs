07949413a8a9bbe3eb1e0517c2c044ef91a7a19e
Marco Perone <pasafama@gmail.com>
2020-02-04 12:44:31 +0100

    [#328] [stbx-core] remove unused imports

--- stbx-core/src/Statebox/Core/Firing.purs
+++ stbx-core/src/Statebox/Core/Firing.purs
@@ -2,9 +2,7 @@ module Statebox.Core.Firing
   ( Firing
   ) where
 
-import Prelude (class Eq, class Show, show, (<>))
 import Data.Maybe (Maybe)
-import Data.Newtype (class Newtype)
 
 import Statebox.Core.Wiring (GluedTransitionIdRaw)
 import Statebox.Core.Common (Message, Singleton, TxId)
