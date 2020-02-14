52d55f6b0bac46bf834ba6ae7c2d3af43ff5a548
Marco Perone <pasafama@gmail.com>
2020-02-14 18:43:09 +0100

    [#336] [stbx-protocol] add first successful firing tests

--- stbx-protocol/test/Statebox/Protocol/Fire.purs
+++ stbx-protocol/test/Statebox/Protocol/Fire.purs
@@ -4,9 +4,11 @@ import Prelude
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Data.NonEmpty (NonEmpty(..))
+import Data.Tuple.Nested ((/\))
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
+import Data.Petrinet.Representation.Marking (fromFoldable)
 import Data.Petrinet.Representation.NLL (ErrNetEncoding(..))
 import Statebox.Core.Wiring (NetsAndDiagramsIndex(..))
 import Statebox.Core.WiringTree (LinearizationError(..))
@@ -14,7 +16,7 @@ import Statebox.Protocol.Fire (FiringError(..), fire)
 
 suite :: Spec Unit
 suite = do
-  describe "Firing a firing" do
+  describe "Firing" do
     it "fails if the wiring is not valid" do
       -- the only relevant thing here is that the wiring is not valid
       let wiring = { diagrams: [{ name: "diagram"
@@ -91,3 +93,43 @@ suite = do
                    }
           marking = mempty
       fire wiring firing marking `shouldEqual` Left FireTransitionNotEnabled
+    it "suceeds for an initial transition" do
+      let wiring = { diagrams: [{ name: "diagram"
+                                , names: []
+                                , pixels: [1]
+                                , width: 1
+                                }]
+                   , nets: [{ name: "net"
+                            , names: ["a"]
+                            -- _ -> 1
+                            , partition: [0, 1, 0]
+                            , placeNames: Nothing
+                            }]
+                   , labels: [NetsAndDiagramsIndex 0]
+                   }
+          firing = { execution: Nothing
+                   , message: Nothing
+                   , path: NonEmpty 0 []
+                   }
+          marking = mempty
+      fire wiring firing marking `shouldEqual` Right (fromFoldable [1 /\ 1])
+    it "suceeds for a normal transition" do
+      let wiring = { diagrams: [{ name: "diagram"
+                                , names: []
+                                , pixels: [1]
+                                , width: 1
+                                }]
+                   , nets: [{ name: "net"
+                            , names: ["a"]
+                            -- 1 -> 2
+                            , partition: [1, 0, 2, 0]
+                            , placeNames: Nothing
+                            }]
+                   , labels: [NetsAndDiagramsIndex 0]
+                   }
+          firing = { execution: Nothing
+                   , message: Nothing
+                   , path: NonEmpty 0 []
+                   }
+          marking = fromFoldable [1 /\ 1]
+      fire wiring firing marking `shouldEqual` Right (fromFoldable [2 /\ 1])
