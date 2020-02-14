1eab4ee674cea9abf2a61249d8151a10e19191f8
Marco Perone <pasafama@gmail.com>
2020-02-14 16:26:28 +0100

    [#336] [stbx-protocol] test that firing fails if wiring is not valid

--- stbx-protocol/test/Statebox/Protocol/Fire.purs
+++ stbx-protocol/test/Statebox/Protocol/Fire.purs
@@ -1,11 +1,31 @@
 module Test.Statebox.Protocol.Fire where
 
 import Prelude
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Data.NonEmpty (NonEmpty(..))
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
+
+import Statebox.Core.Wiring (NetsAndDiagramsIndex(..))
+import Statebox.Protocol.Fire (FiringError(..), fire)
 
 suite :: Spec Unit
 suite = do
   describe "Firing a firing" do
-    it "should work" do
-      1 `shouldEqual` 1
+    it "fails if the wiring is not valid" do
+      -- the only relevant thing here is that the wiring is not valid
+      let wiring = { diagrams: [{ name: "diagram"
+                                , names: []
+                                , pixels: [1, 2]
+                                , width: 1
+                                }]
+                   , nets : []
+                   , labels : [NetsAndDiagramsIndex 5]
+                   }
+          firing = { execution : Nothing
+                   , message: Nothing
+                   , path: NonEmpty 3 []
+                   }
+          marking = mempty
+      fire wiring firing marking `shouldEqual` Left FireInvalidWiringTree
