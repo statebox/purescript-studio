ee4c97edf589e524874adb556fbd3e52280e2c77
Marco Perone <pasafama@gmail.com>
2020-02-21 18:22:52 +0100

    [#360] [stbx-protocol] eval MultipleStoresActions in terms of evaluations of the single stores

--- stbx-protocol/src/Statebox/Protocol/Store/Hadler.purs
+++ stbx-protocol/src/Statebox/Protocol/Store/Hadler.purs
@@ -1,22 +1,34 @@
 module Statebox.Protocol.Store.Handler where
 
 import Prelude
-import Control.Monad.Free (Free, hoistFree)
+import Control.Monad.Free (Free, hoistFree, runFreeM)
+import Control.Monad.Rec.Class (class MonadRec)
 
 import Statebox.Core.Transaction (TxSum, TxId)
 import Statebox.Protocol.ExecutionState (ExecutionState)
 import Statebox.Protocol.Store (StoreActions, StoreActionF(..))
 import Statebox.Store (Actions, get, put)
 
-data MultipleStoresAction a
+data MultipleStoresActionF a
   = Transaction    (Actions TxId TxSum          a)
   | ExecutionState (Actions TxId ExecutionState a)
 
-type MultipleStoresActions = Free MultipleStoresAction
+derive instance multipleStoresActionFunctor :: Functor MultipleStoresActionF
 
-eval :: forall a. StoreActions a -> MultipleStoresActions a
-eval = hoistFree (case _ of
+type MultipleStoresActions = Free MultipleStoresActionF
+
+hoistToMultipleStores :: forall a. StoreActions a -> MultipleStoresActions a
+hoistToMultipleStores = hoistFree (case _ of
   GetTransaction       key       next -> Transaction (next <$> get key)
   PutTransaction       key value next -> Transaction (next <$ put key value)
   GetExecutionState    key       next -> ExecutionState (next <$> get key)
   UpdateExecutionState key value next -> ExecutionState (next <$ put key value))
+
+eval
+  :: forall a m. MonadRec m
+  => (forall b. Actions TxId TxSum b          -> m b)
+  -> (forall c. Actions TxId ExecutionState c -> m c)
+  -> MultipleStoresActions a        -> m a
+eval evalTransactions evalExecutionStates = runFreeM $ case _ of
+  Transaction    transactionActions    -> evalTransactions    transactionActions
+  ExecutionState executionStateActions -> evalExecutionStates executionStateActions
