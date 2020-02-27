6dd9ea84f8c600391d589bf19643c41e1de7692e
Marco Perone <pasafama@gmail.com>
2020-02-27 08:52:40 +0100

    [#360] [stbx-core] use .!= to provide default value

--- stbx-core/src/Statebox/Core/Transaction/Codec.purs
+++ stbx-core/src/Statebox/Core/Transaction/Codec.purs
@@ -5,14 +5,14 @@ import Control.Alt ((<|>))
 import Data.Argonaut.Core (Json, jsonEmptyObject)
 import Data.Argonaut.Encode.Combinators ((:=), (~>))
 import Data.Argonaut.Encode.Class (encodeJson)
-import Data.Argonaut.Decode (decodeJson, (.:), (.:?))
+import Data.Argonaut.Decode (decodeJson, (.:), (.:?), (.!=))
 import Data.Argonaut.Decode.Class (decodeJArray)
-import Data.Lens (over)
-import Data.Maybe (fromMaybe, maybe)
-import Data.Profunctor.Choice (left)
 import Data.Either (Either(..))
 import Data.Either.Nested (type (\/))
+import Data.Lens (over)
+import Data.Maybe (maybe)
 import Data.NonEmpty (singleton)
+import Data.Profunctor.Choice (left)
 import Data.Traversable (traverse)
 import Foreign.Object (Object, lookup)
 
@@ -50,10 +50,9 @@ decodeTxFiringTx = decodeTxWith decodeFiringTx <=< decodeJson
 -- | from the JSON input. The "z" value is the `uberRootHash`.
 decodeInitialTx :: Json -> String \/ InitialTx
 decodeInitialTx = decodeJson >=> \x -> do
-  root          <- x .: "root"
-  previousMaybe <- x .:? "previous"
+  root     <- x .: "root"
   -- if we encounter JSON without a "previous" field in the root's "decoded" payload, insert it artificially
-  let previous   = fromMaybe uberRootHash previousMaybe
+  previous <- x .:? "previous" .!=  uberRootHash
   pure { root, previous }
 
 decodeWiringTx :: Json -> String \/ WiringTx
