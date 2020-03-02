09c519626d7906969f572f4c3744a40b04173fd3
Erik Post <erik@shinsetsu.nl>
2020-03-02 23:33:40 +0100

    [#360] [stbx-store] fix typo

--- stbx-store/src/Statebox/Store/ConsoleLogger.purs
+++ stbx-store/src/Statebox/Store/ConsoleLogger.purs
@@ -10,7 +10,7 @@ import Effect.Class.Console (log)
 
 import Statebox.Store.Types (ActionF(Get, Put), Actions)
 
--- | Interpreter storage actions as console 'log's.
+-- | Interpret storage actions as console 'log's.
 eval :: forall k v a m. Show k => Show v => MonadEffect m => MonadRec m => Actions k v a -> m a
 eval = runFreeM $ \action -> case action of
   Get key next -> do
