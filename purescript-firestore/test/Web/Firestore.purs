2ab5849ee293843cd83582f08341f63ab96c63ac
Marco Perone <pasafama@gmail.com>
2020-03-05 11:17:01 +0100

    [#231] [purescript-firestore] setup firestore project with statebox account

--- purescript-firestore/test/Web/Firestore.purs
+++ purescript-firestore/test/Web/Firestore.purs
@@ -15,16 +15,16 @@ suite :: Spec Unit
 suite = do
   describe "Firestore" do
     it "sets and gets data correctly" do
-      let options = { apiKey: Just "AIzaSyBFgU7cDEBkYmRCnj7hSLbNEj6LNzfwIew"
-                    , appId: Just "1:630616051437:web:118b8ede4a1a86c73b46e4"
-                    , authDomain: Just "marcoshtest-b082d.firebaseapp.com"
-                    , databaseUrl: Just "https://marcoshtest-b082d.firebaseio.com"
+      let options = { apiKey: Just "AIzaSyBDH92I_Qiv_GHbIOA0MddiOKZpwDaMNoY"
+                    , appId: Just "1:490707848264:web:92683957e61378c9a21c7d"
+                    , authDomain: Just "firestore-test-270209.firebaseapp.com"
+                    , databaseUrl: Just "https://firestore-test-270209.firebaseio.com"
                     , measurementId: Nothing
-                    , messagingSenderId: Just "630616051437"
-                    , projectId: "marcoshtest-b082d"
-                    , storageBucket: Just "marcoshtest-b082d.appspot.com"
+                    , messagingSenderId: Just "490707848264"
+                    , projectId: "firestore-test-270209"
+                    , storageBucket: Just "firestore-test-270209.appspot.com"
                     }
-          app = initializeApp options (Just "marcoshtest")
+          app = initializeApp options (Just "firestore-test")
           firestoreInstance = firestore app
           documentReference = doc firestoreInstance "collection/test"
           document = DocumentData (singleton "foo" (PrimitiveDocument (PVText "bar")))
