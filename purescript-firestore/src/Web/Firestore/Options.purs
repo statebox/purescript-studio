636b9ab816269ec0f57c4972e67fb011170226c7
Marco Perone <pasafama@gmail.com>
2020-03-10 16:26:47 +0100

    [#231] [purescript-firestore] add Options EncodeJson instance and use Lens' instead of Traversal'

--- purescript-firestore/src/Web/Firestore/Options.purs
+++ purescript-firestore/src/Web/Firestore/Options.purs
@@ -1,7 +1,8 @@
 module Web.Firestore.Options where
 
-import Prelude ((>>>))
-import Data.Lens (Iso', Lens', Traversal', _Just, iso, lens)
+import Prelude
+import Data.Argonaut (class EncodeJson, jsonEmptyObject, (:=), (:=?), (~>), (~>?))
+import Data.Lens (Iso', Lens', iso, lens, view)
 import Data.Maybe (Maybe(..))
 
 newtype Options = Options
@@ -14,6 +15,24 @@ newtype Options = Options
   , projectId :: String
   , storageBucket :: Maybe String
   }
+
+instance showOptions :: Show Options where
+  show (Options r) = "Options: " <> show r
+
+instance eqOptions :: Eq Options where
+  eq (Options r) (Options r') = eq r r'
+
+instance encodeJsonOptions :: EncodeJson Options where
+  encodeJson opt
+    =   "apiKey"            :=? view apiKey            opt
+    ~>? "appId"             :=? view appId             opt
+    ~>? "authDomain"        :=? view authDomain        opt
+    ~>? "databaseUrl"       :=? view databaseUrl       opt
+    ~>? "measurementId"     :=? view measurementId     opt
+    ~>? "messagingSenderId" :=? view messagingSenderId opt
+    ~>? "projectId"         :=  view projectId         opt
+    ~>  "storageBucket"     :=? view storageBucket     opt
+    ~>? jsonEmptyObject
 
 options :: String -> Options
 options pId = Options
@@ -38,26 +57,26 @@ optionsIso :: Iso' Options { apiKey :: Maybe String
                            }
 optionsIso = iso (\(Options r) -> r) Options
 
-apiKey :: Traversal' Options String
-apiKey = _Just >>> lens (_.apiKey) (_ {apiKey = _}) >>> optionsIso
+apiKey :: Lens' Options (Maybe String)
+apiKey = lens (_.apiKey) (_ {apiKey = _}) >>> optionsIso
 
-appId :: Traversal' Options String
-appId = _Just >>> lens (_.appId) (_ {appId = _}) >>> optionsIso
+appId :: Lens' Options (Maybe String)
+appId = lens (_.appId) (_ {appId = _}) >>> optionsIso
 
-authDomain :: Traversal' Options String
-authDomain = _Just >>> lens (_.authDomain) (_ {authDomain = _}) >>> optionsIso
+authDomain :: Lens' Options (Maybe String)
+authDomain = lens (_.authDomain) (_ {authDomain = _}) >>> optionsIso
 
-databaseUrl :: Traversal' Options String
-databaseUrl = _Just >>> lens (_.databaseUrl) (_ {databaseUrl = _}) >>> optionsIso
+databaseUrl :: Lens' Options (Maybe String)
+databaseUrl = lens (_.databaseUrl) (_ {databaseUrl = _}) >>> optionsIso
 
-measurementId :: Traversal' Options String
-measurementId = _Just >>> lens (_.measurementId) (_ {measurementId = _}) >>> optionsIso
+measurementId :: Lens' Options (Maybe String)
+measurementId = lens (_.measurementId) (_ {measurementId = _}) >>> optionsIso
 
-messagingSenderId :: Traversal' Options String
-messagingSenderId = _Just >>> lens (_.messagingSenderId) (_ {messagingSenderId = _}) >>> optionsIso
+messagingSenderId :: Lens' Options (Maybe String)
+messagingSenderId = lens (_.messagingSenderId) (_ {messagingSenderId = _}) >>> optionsIso
 
 projectId :: Lens' Options String
 projectId = lens (_.projectId) (_ {projectId = _}) >>> optionsIso
 
-storageBucket :: Traversal' Options String
-storageBucket = _Just >>> lens (_.storageBucket) (_ {storageBucket = _}) >>> optionsIso
+storageBucket :: Lens' Options (Maybe String)
+storageBucket = lens (_.storageBucket) (_ {storageBucket = _}) >>> optionsIso
