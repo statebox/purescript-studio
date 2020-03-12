8f1e59279ac23845927a15e22c7085841f096dbf
Marco Perone <pasafama@gmail.com>
2020-03-12 18:40:40 +0100

    [#231] [purescript-firestore] read configuration parameters from .env

--- purescript-firestore/test/Web/FirestoreSpec.purs
+++ purescript-firestore/test/Web/FirestoreSpec.purs
@@ -5,7 +5,10 @@ import Control.Promise (toAff)
 import Data.Lens as Lens
 import Data.Maybe (Maybe(..))
 import Data.Tuple.Nested ((/\))
+import Dotenv (loadFile) as Dotenv
+import Effect.Class (liftEffect)
 import Foreign.Object (fromFoldable)
+import Node.Process (lookupEnv)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (fail, shouldEqual)
 
@@ -21,28 +24,39 @@ suite :: Spec Unit
 suite = do
   describe "Firestore" do
     it "sets and gets data correctly" do
-      let fsOptions = options "firestore-test-270209"
-                    # Lens.set apiKey            (Just "AIzaSyBDH92I_Qiv_GHbIOA0MddiOKZpwDaMNoY")
-                    # Lens.set appId             (Just "1:490707848264:web:92683957e61378c9a21c7d")
-                    # Lens.set authDomain        (Just "firestore-test-270209.firebaseapp.com")
-                    # Lens.set databaseUrl       (Just "https://firestore-test-270209.firebaseio.com")
-                    # Lens.set messagingSenderId (Just "490707848264")
-                    # Lens.set storageBucket     (Just "firestore-test-270209.appspot.com")
-          app = initializeApp fsOptions (Just "firestore-test")
-          firestoreInstance = firestore app
-          maybeDocumentReference = doc firestoreInstance <$> (pathFromString "collection/test")
-      case maybeDocumentReference of
-        Nothing                -> fail "invalid path"
-        Just documentReference ->
-          let document = DocumentData (fromFoldable [ "text"    /\ (PrimitiveDocument (PVText    "some text"))
-                                                    , "integer" /\ (PrimitiveDocument (PVInteger 42         ))
-                                                    , "float"   /\ (PrimitiveDocument (PVFloat   273.15     ))
-                                                    , "bool"    /\ (PrimitiveDocument (PVBoolean true       ))])
-              setPromise = set documentReference document (Just $ mergeFieldsOption [stringMergeField "text", fieldPathMergeField ["float"]])
-              getPromise = get documentReference Nothing
-          in do
-            toAff setPromise
-            snapshot <- toAff getPromise
-            let result = snapshotData snapshot Nothing
+      _ <- Dotenv.loadFile
+      firestoreProjectId         <- liftEffect $ lookupEnv "FIRESTORE_PROJECT_ID"
+      firestoreApiKey            <- liftEffect $ lookupEnv "FIRESTORE_API_KEY"
+      firestoreAppId             <- liftEffect $ lookupEnv "FIRESTORE_APP_ID"
+      firestoreAuthDomain        <- liftEffect $ lookupEnv "FIRESTORE_AUTH_DOMAIN"
+      firestoreDatabaseUrl       <- liftEffect $ lookupEnv "FIRESTORE_DATABASE_URL"
+      firestoreMessagingSenderId <- liftEffect $ lookupEnv "FIRESTORE_MESSAGING_SENDER_ID"
+      firestoreStorageBucket     <- liftEffect $ lookupEnv "FIRESTORE_STORAGE_BUCKET"
+      case firestoreProjectId of
+        Nothing        -> fail "invalid project id"
+        Just projectId -> do
+          let fsOptions = options projectId
+                        # Lens.set apiKey            firestoreApiKey
+                        # Lens.set appId             firestoreAppId
+                        # Lens.set authDomain        firestoreAuthDomain
+                        # Lens.set databaseUrl       firestoreDatabaseUrl
+                        # Lens.set messagingSenderId firestoreMessagingSenderId
+                        # Lens.set storageBucket     firestoreStorageBucket
+              app = initializeApp fsOptions (Just "firestore-test")
+              firestoreInstance = firestore app
+              maybeDocumentReference = doc firestoreInstance <$> (pathFromString "collection/test")
+          case maybeDocumentReference of
+            Nothing                -> fail "invalid path"
+            Just documentReference ->
+              let document = DocumentData (fromFoldable [ "text"    /\ (PrimitiveDocument (PVText    "some text"))
+                                                        , "integer" /\ (PrimitiveDocument (PVInteger 42         ))
+                                                        , "float"   /\ (PrimitiveDocument (PVFloat   273.15     ))
+                                                        , "bool"    /\ (PrimitiveDocument (PVBoolean true       ))])
+                  setPromise = set documentReference document (Just $ mergeFieldsOption [stringMergeField "text", fieldPathMergeField ["float"]])
+                  getPromise = get documentReference Nothing
+              in do
+                toAff setPromise
+                snapshot <- toAff getPromise
+                let result = snapshotData snapshot Nothing
 
-            result `shouldEqual` Just document
+                result `shouldEqual` Just document
